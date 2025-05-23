693c52d6b0c8d013b719979f58781c735f2fde33
Hao Chen <chenhaox@outlook.com>
2025-05-23 10:21:43 +0800

    Update voiceChat.ts

--- frontend/src/services/voiceChat.ts
+++ frontend/src/services/voiceChat.ts
@@ -99,8 +99,8 @@ class VoiceChat {
         config: {
           iceServers: [
             { urls: 'stun:stun.miwifi.com:3478' },
-      { urls: 'stun:stun.chat.bilibili.com:3478' },
-      { urls: 'stun:stun.cloudflare.com:3478' }
+            { urls: 'stun:stun.chat.bilibili.com:3478' },
+            { urls: 'stun:stun.cloudflare.com:3478' },
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' }
           ]
